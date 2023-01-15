a4598937bcd324c20db8f023723deec3ee0de1c5
Oereor <joseph0127@outlook.com>
2023-01-15 11:02:11 +0800

    调试后自动产生的文件

--- Ink/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ Ink/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -232,7 +232,7 @@ namespace Ink {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Ink;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Ink;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
